134f1763bf0f8f738c5b0f375c2442ec30fe7496
hafsaahad22 <261947251@formanite.fccollege.edu.pk>
2025-05-25 23:57:50 +0500

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,10 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/Home";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("@/pages/Home"));
+
 import NotFound from "@/pages/not-found";
 
 function Router() {
